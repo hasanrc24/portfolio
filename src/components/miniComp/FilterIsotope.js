01c89cfd858145ce5ac4029804b4536a2268a6d0
hasanrc24 <hasanrc24@gmail.com>
2023-02-05 00:29:23 +0600

    tab sort

--- src/components/miniComp/FilterIsotope.js
+++ src/components/miniComp/FilterIsotope.js
@@ -28,27 +28,67 @@ const IsotopeReact = () => {
 
   return (
     <>
-      <ul>
-        <li onClick={handleFilterKeyChange("*")}>All</li>
-        <li onClick={handleFilterKeyChange("react")}>React.js</li>
-        <li onClick={handleFilterKeyChange("next")}>Next.js</li>
-        <li onClick={handleFilterKeyChange("type")}>TypeScript</li>
-      </ul>
-      <hr />
-      <ul className="filter-container">
-        <div className="filter-item react">
-          <span>Cucumber</span>
+      <div className="flex justify-center gap-6 font-medium">
+        <span className=" cursor-pointer" onClick={handleFilterKeyChange("*")}>
+          All
+        </span>
+        <span
+          className=" cursor-pointer"
+          onClick={handleFilterKeyChange("react")}
+        >
+          React.js
+        </span>
+        <span
+          className=" cursor-pointer"
+          onClick={handleFilterKeyChange("next")}
+        >
+          Next.js
+        </span>
+        <span
+          className=" cursor-pointer"
+          onClick={handleFilterKeyChange("type")}
+        >
+          TypeScript
+        </span>
+      </div>
+      <div className="filter-container m-auto">
+        <div className="filter-item m-4 next type">
+          <div className="cursor-pointer rounded-lg max-h-48 w-72 overflow-hidden">
+            <img
+              className=" duration-1000"
+              src="/images/portfolio/next-blog-pic.png"
+              alt="next-blog"
+            />
+          </div>
         </div>
-        <div className="filter-item next">
-          <span>Apple</span>
+        <div className="filter-item m-4 react">
+          <div className="cursor-pointer rounded-lg max-h-48 w-72 overflow-hidden">
+            <img
+              className=" hover:-translate-y-44 transition-all duration-1000"
+              src="/images/portfolio/e-com-pic.png"
+              alt="e-com"
+            />
+          </div>
         </div>
-        <div className="filter-item type">
-          <span>Orange</span>
+        <div className="filter-item m-4 react">
+          <div className="cursor-pointer rounded-lg max-h-48 w-72 overflow-hidden">
+            <img
+              className=" hover:-translate-y-72 transition-all duration-1000"
+              src="/images/portfolio/creative-agency-pic.png"
+              alt="e-com"
+            />
+          </div>
         </div>
-        <div className="filter-item react next">
-          <span>Tomato</span>
+        <div className="filter-item m-4 react">
+          <div className="cursor-pointer rounded-lg max-h-48 w-72 overflow-hidden">
+            <img
+              className=" hover:-translate-y-72 transition-all duration-1000"
+              src="/images/portfolio/doctors-portal-pic.png"
+              alt="e-com"
+            />
+          </div>
         </div>
-      </ul>
+      </div>
     </>
   );
 };
