9477ede41e54937eb913a63073c9227a8983376c
hasanrc24 <hasanrc24@gmail.com>
2023-02-11 18:07:19 +0600

    fi

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import { FaFacebookF, FaTwitter, FaLinkedinIn, FaGithub } from "react-icons/fa";
 
 const Nav = () => {
-  const [toggleOpen, setToggleOpen] = useState(true);
+  const [toggleOpen, setToggleOpen] = useState(false);
   return (
-    <nav className="px-6 py-4 md:p-0 bg-nav-color z-10 text-white flex justify-between items-center md:flex-col flex-row lg:h-screen md:py-10">
+    <nav className="px-6 py-4 bg-nav-color z-30 text-white flex justify-between items-center md:flex-col flex-row lg:h-screen md:py-10">
       <div className="md:flex md:flex-col md:justify-center">
         <div className="h-44 w-44 hidden md:block rounded-full overflow-hidden border-8 border-img-border">
           <img src="/images/profile.jpg" alt="avatar" />
@@ -13,44 +13,56 @@ const Nav = () => {
           Md Hasan
         </a>
       </div>
-      <div className="hidden md:flex md:flex-col  justify-center items-center gap-5 mb-8">
+      {/* <div className="hidden md:flex md:flex-col justify-center items-center gap-5 mb-8"> */}
+      <div
+        className={`-z-10 transition-all duration-300 absolute md:static md:z-10 left-0 py-6 md:py-0 w-full flex flex-col bg-nav-color justify-center items-center gap-5 md:mb-8 ${
+          toggleOpen ? " top-16" : " -top-96"
+        }`}
+      >
         <a
+          onClick={() => setToggleOpen(!toggleOpen)}
           href="#home"
           className="hover:text-brand-color transition duration-250 ease-linear"
         >
           Home
         </a>
         <a
+          onClick={() => setToggleOpen(!toggleOpen)}
           href="#about"
           className="hover:text-brand-color transition duration-250 ease-linear"
         >
           About Me
         </a>
         <a
+          onClick={() => setToggleOpen(!toggleOpen)}
           href="#services"
           className="hover:text-brand-color transition duration-250 ease-linear"
         >
           What I Do
         </a>
         <a
+          onClick={() => setToggleOpen(!toggleOpen)}
           href="#resume"
           className="hover:text-brand-color transition duration-250 ease-linear"
         >
           Resume
         </a>
         <a
+          onClick={() => setToggleOpen(!toggleOpen)}
           href="#portfolio"
           className="hover:text-brand-color transition duration-250 ease-linear"
         >
           Portfolio
         </a>
         <a
+          onClick={() => setToggleOpen(!toggleOpen)}
           href="#testimonial"
           className="hover:text-brand-color transition duration-250 ease-linear"
         >
           Testimonial
         </a>
         <a
+          onClick={() => setToggleOpen(!toggleOpen)}
           href="#contact"
           className="hover:text-brand-color transition duration-250 ease-linear"
         >
